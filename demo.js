7132c761906f6a762b5ff4a16b688a3cacf220cd
MohdTazammul <tazammul1683@gmail.com>
2021-12-03 16:41:23 +0530

    Json data correction

---
+++ demo.js
@@ -0,0 +1,1000 @@
+les Escaldes
+Andorra la Vella
+Umm al Qaywayn
+Ras al-Khaimah
+Khawr Fakkān
+Dubai
+Dibba Al-Fujairah
+Dibba Al-Hisn
+Sharjah
+Ar Ruways
+Al Fujayrah
+Al Ain
+Ajman
+Adh Dhayd
+Abu Dhabi
+Zaranj
+Taloqan
+Shīnḏanḏ
+Shibirghān
+Shahrak
+Sar-e Pul
+Sang-e Chārak
+Aībak
+Rustāq
+Qarqīn
+Qarāwul
+Pul-e Khumrī
+Paghmān
+Nahrīn
+Maymana
+Mehtar Lām
+Mazār-e Sharīf
+Lashkar Gāh
+Kushk
+Kunduz
+Khōst
+Khulm
+Khāsh
+Khanabad
+Karukh
+Kandahār
+Kabul
+Jalālābād
+Jabal os Saraj
+Herāt
+Ghormach
+Ghazni
+Gereshk
+Gardēz
+Fayzabad
+Farah
+Kafir Qala
+Charikar
+Barakī Barak
+Bāmyān
+Balkh
+Baghlān
+Ārt Khwājah
+Āsmār
+Asadābād
+Andkhōy
+Bāzārak
+Markaz-e Woluswalī-ye Āchīn
+Saint John’s
+The Valley
+Sarandë
+Kukës
+Korçë
+Gjirokastër
+Elbasan
+Burrel
+Vlorë
+Tirana
+Shkodër
+Patos Fshat
+Lushnjë
+Lezhë
+Laç
+Kuçovë
+Krujë
+Kavajë
+Fier-Çifçi
+Fier
+Durrës
+Berat
+Kapan
+Goris
+Hats’avan
+Artashat
+Ararat
+Yerevan
+Ejmiatsin
+Spitak
+Sevan
+Masis
+Vanadzor
+Gavarr
+Hrazdan
+Armavir
+Gyumri
+Ashtarak
+Abovyan
+Saurimo
+Lucapa
+Luau
+Uíge
+Soio
+Nzeto
+N’dalatando
+Mbanza Congo
+Malanje
+Luanda
+Caxito
+Cabinda
+Sumbe
+Namibe
+Menongue
+Luena
+Lubango
+Longonjo
+Lobito
+Cuito
+Huambo
+Catumbela
+Catabola
+Camacupa
+Caluquembe
+Caála
+Benguela
+Zárate
+Villa Ocampo
+Villa Lugano
+Villaguay
+Villa Gesell
+Tigre
+Tandil
+San Vicente
+Santo Tomé
+Santa Elena
+San Pedro
+San Luis del Palmar
+San Lorenzo
+San Javier
+San Isidro
+Saladas
+Retiro
+Resistencia
+Reconquista
+Quilmes
+Puerto Rico
+Puerto Iguazú
+Puerto Esperanza
+Puerto Eldorado
+Posadas
+Pontevedra
+Pirané
+Paso de los Libres
+Oberá
+Necochea
+Morón
+Monte Caseros
+Montecarlo
+Mercedes
+Mercedes
+Mar del Plata
+Luján
+La Plata
+La Paz
+Jardín América
+Gualeguaychú
+Gualeguay
+Goya
+Gobernador Ingeniero Valentín Virasoro
+General José de San Martín
+Garupá
+Formosa
+Fontana
+Federal
+Esquina
+El Soberbio
+Dolores
+Curuzú Cuatiá
+Corrientes
+Concordia
+Concepción del Uruguay
+Colegiales
+Chajarí
+Campana
+Buenos Aires
+Barranqueras
+Azul
+Avellaneda
+Aristóbulo del Valle
+Zapala
+Yerba Buena
+Villa Regina
+Villa Paula de Sarmiento
+Villa Nueva
+Villa María
+Villa Dolores
+Villa Constitución
+Villa Carlos Paz
+Villa Ángela
+Villa Allende
+Viedma
+Victoria
+Vera
+Venado Tuerto
+Veinticinco de Mayo
+Ushuaia
+Unquillo
+Tres Isletas
+Tres Arroyos
+Trelew
+Termas de Río Hondo
+Tartagal
+Tafí Viejo
+Sunchales
+Santo Tomé
+Santiago del Estero
+Santa Rosa
+Santa Lucía
+Santa Fe de la Vera Cruz
+San Salvador de Jujuy
+San Ramón de la Nueva Orán
+San Rafael
+San Pedro
+San Nicolás de los Arroyos
+San Miguel de Tucumán
+San Martín de los Andes
+San Martín
+San Luis
+San Justo
+San Juan
+San José de Jáchal
+San Jorge
+San Francisco
+San Fernando del Valle de Catamarca
+San Antonio Oeste
+Salta
+Rufino
+Rosario
+Río Tercero
+Río Segundo
+Río Gallegos
+Río Cuarto
+Río Ceballos
+Rawson
+Rafaela
+Quitilipi
+Punta Alta
+Puerto Madryn
+Presidencia Roque Sáenz Peña
+Pocito
+Plottier
+Pergamino
+Pérez
+Paraná
+Palpalá
+Olavarría
+Nueve de Julio
+Neuquén
+Morteros
+Monteros
+Mendoza
+Marcos Juárez
+Machagai
+Lincoln
+Libertador General San Martín
+Las Breñas
+La Rioja
+La Falda
+La Calera
+Laboulaye
+Junín
+Joaquín V. González
+Jesús María
+Granadero Baigorria
+Gobernador Gálvez
+General Roca
+General Pinedo
+General Pico
+General Enrique Mosconi
+Gálvez
+Firmat
+Famaillá
+Esquel
+Esperanza
+Embarcación
+Embalse
+El Bolsón
+Diamante
+Deán Funes
+Cutral-Có
+Cruz del Eje
+Crespo
+Cosquín
+Coronel Suárez
+Coronda
+Córdoba
+Comodoro Rivadavia
+Cipolletti
+Cinco Saltos
+Chivilcoy
+Chimbas
+Chilecito
+Charata
+Chacabuco
+Centenario
+Caucete
+Catriel
+Castelli
+Casilda
+Carcarañá
+Capitán Bermúdez
+Cañada de Gómez
+Caleta Olivia
+Bell Ville
+Bella Vista
+Bahía Blanca
+Arroyo Seco
+Arroyito
+Añatuya
+Alta Gracia
+Allen
+Alderetes
+Albardón
+Aguilares
+Villa Santa Rita
+Villa Mercedes
+San Carlos de Bariloche
+Adrogué
+Pago Pago
+Wolfsberg
+Wiener Neustadt
+Vienna
+Wels
+Weinzierl bei Krems
+Villach
+Traun
+Traiskirchen
+Ternitz
+Steyr
+Spittal an der Drau
+Schwechat
+Sankt Pölten
+Salzburg
+Saalfelden am Steinernen Meer
+Mödling
+Lustenau
+Linz
+Leonding
+Leoben
+Kufstein
+Krems an der Donau
+Klosterneuburg
+Klagenfurt am Wörthersee
+Kapfenberg
+Innsbruck
+Hallein
+Graz
+Feldkirch
+Dornbirn
+Bregenz
+Braunau am Inn
+Baden
+Amstetten
+Ansfelden
+Whyalla
+Rockingham
+Prospect
+Port Hedland
+Perth
+Murray Bridge
+Mount Isa
+Morphett Vale
+Mandurah
+Kwinana
+Kalgoorlie
+Gosnells
+Geraldton
+Gawler
+Fremantle
+Darwin
+Busselton
+Bunbury
+Armadale
+Alice Springs
+Albany
+Adelaide
+Woodridge
+Wodonga
+Werribee
+Warrnambool
+Wantirna South
+Wangaratta
+Wagga Wagga
+Traralgon
+Townsville
+Toowoomba
+Thornbury
+Thomastown
+Tarneit
+Taree
+Tamworth
+Sydney
+Surfers Paradise
+Sunnybank
+Sunbury
+Port Stephens
+Springvale
+Southport
+South Grafton
+Shepparton
+Seaford
+Saint Kilda
+Saint Albans
+Rowville
+Rockhampton
+Richmond
+Reservoir
+Queanbeyan
+Quakers Hill
+Preston
+Port Macquarie
+Point Cook
+Pakenham South
+Orange
+Nowra
+Northcote
+Noble Park
+Newcastle
+Nerang
+Narre Warren
+Narangba
+Mulgrave
+Mount Martha
+Mount Gambier
+Mount Eliza
+Mosman
+Mornington
+Morayfield
+Moe
+Mill Park
+Mildura
+Melton
+Melbourne
+Maryborough
+Marrickville
+Maroubra
+Maitland
+Mackay
+Liverpool
+Lismore
+Lilydale
+Launceston
+Lara
+Langwarrin
+Lalor
+Keysborough
+Kew
+Katoomba
+Hornsby
+Hobart
+Hawthorn South
+Hampton Park
+Griffith
+Greensborough
+Granville
+Goulburn
+Gold Coast
+Glenroy
+Glenferrie
+Gladstone
+Geelong
+Frankston East
+Frankston
+Forster
+Essendon
+Epping
+Epping
+Engadine
+Eltham
+Echuca
+Earlwood
+Dubbo
+Doncaster East
+Doncaster
+Devonport
+Deer Park
+Deception Bay
+Dandenong
+Cronulla
+Cranbourne
+Cranbourne
+Craigieburn
+Coffs Harbour
+Coburg
+Clayton
+Wollongong
+Cessnock
+Castle Hill
+Carrum Downs
+Carnegie
+Carlingford
+Caringbah
+Canberra
+Camberwell
+Caloundra
+Cairns
+Caboolture
+Burnie
+Bundaberg
+Buderim
+Brunswick
+Broken Hill
+Brisbane
+Boronia
+Blacktown
+Berwick
+Bendigo
+Baulkham Hills
+Bathurst
+Banora Point
+Bankstown
+Ballarat
+Auburn
+Ashfield
+Armidale
+Albury
+South Brisbane
+Cheltenham
+Randwick
+Dee Why
+Umina
+Palmerston
+Bracken Ridge
+North Ryde
+Hoppers Crossing
+Logan City
+Carindale
+Paramatta
+Ferntree Gully
+City of Parramatta
+Adelaide Hills
+Canning Vale
+Glenmore Park
+Glen Iris
+Balwyn North
+Carnegie
+Malvern East
+Brighton East
+Booval
+St Albans
+Endeavour Hills
+Clayton
+Taylors Lakes
+Roxburgh Park
+Wyndham Vale
+Willetton
+Thornbury
+Thornlie
+Hillside
+Bundoora
+Forest Lake
+Sunnybank Hills
+Narre Warren South
+Dandenong North
+Frankston South
+Sunshine West
+Altona Meadows
+West Pennant
+Oranjestad
+Babijn
+Angochi
+Mariehamn
+Xankandi
+Əhmədbəyli
+Shushi
+Salyan
+Saatlı
+Neftçala
+Nakhchivan
+Lankaran
+Imishli
+Fizuli
+Dzhalilabad
+Pushkino
+Beylagan
+Astara
+Şirvan
+Ağdam
+Zaqatala
+Zabrat
+Yevlakh
+Yelenendorf
+Xaçmaz
+Ujar
+Terter
+Sumqayıt
+Qaraçuxur
+Shamkhor
+Shamakhi
+Sheki
+Sabunçu
+Sabirabad
+Qusar
+Quba
+Hacıqabul
+Qazax
+Hacı Zeynalabdin
+Mingelchaur
+Maştağa
+Mardakan
+Lökbatan
+Kyurdarmir
+Khirdalan
+Yeni Suraxanı
+Geoktschai
+Hövsan
+Ganja
+Divichibazar
+Buzovna
+Biny Selo
+Barda
+Bilajari
+Baku
+Amirdzhan
+Aghsu
+Ağdaş
+Agdzhabedy
+Bakıxanov
+Zenica
+Visoko
+Velika Kladuša
+Tuzla
+Trebinje
+Travnik
+Sarajevo
+Prijedor
+Mostar
+Gradačac
+Gračanica
+Goražde
+Doboj
+Cazin
+Bugojno
+Brčko
+Bosanska Krupa
+Bijeljina
+Bihać
+Banja Luka
+Konjic
+Bridgetown
+Thākurgaon
+Teknāf
+Tungi
+Sylhet
+Dohār
+Jamālpur
+Shibganj
+Sātkhira
+Sirājganj
+Netrakona
+Narsingdi
+Sandwīp
+Shāhzādpur
+Rāmganj
+Rājshāhi
+Pirojpur
+Panchagarh
+Patiya
+Parbatipur
+Nārāyanganj
+Nālchiti
+Nāgarpur
+Nageswari
+Mymensingh
+Muktāgācha
+Mirzāpur
+Maulavi Bāzār
+Morrelgonj
+Mehendiganj
+Mathba
+Lalmanirhat
+Lākshām
+Comilla
+Rangpur
+Kushtia
+Kālīganj
+Jhingergācha
+Joypur Hāt
+Ishurdi
+Habiganj
+Gaurnadi
+Gafargaon
+Feni
+Rāipur
+Sarankhola
+Dhaka
+Chilmāri
+Chhāgalnāiya
+Lālmohan
+Khagrachhari
+Chhātak
+Bhātpāra Abhaynagar
+Bherāmāra
+Bhairab Bāzār
+Bāndarban
+Kālia
+Baniachang
+Bājitpur
+Badarganj
+Narail
+Tungipāra
+Sarishābāri
+Sakhipur
+Raojān
+Phultala
+Pālang
+Pār Naogaon
+Nabīnagar
+Lakshmīpur
+Kesabpur
+Hājīganj
+Farīdpur
+Dinājpur
+Uttar Char Fasson
+Chittagong
+Char Bhadrāsan
+Bera
+Burhānuddin
+Sātkania
+Cox’s Bāzār
+Khulna
+Bhola
+Barisāl
+Jessore
+Pābna
+Tāngāil
+Bogra
+Pīrgaaj
+Nawābganj
+Mādārīpur
+Sherpur
+Kishorganj
+Manikchari
+Bhāndāria
+Fatikchari
+Saidpur
+Azimpur
+Paltan
+Zwijndrecht
+Zwevegem
+Zottegem
+Zonhoven
+Zoersel
+Zemst
+Zele
+Zedelgem
+Zaventem
+Wuustwezel
+Willebroek
+Wevelgem
+Wetteren
+Westerlo
+Wervik
+Wavre
+Waterloo
+Waregem
+Walcourt
+Visé
+Vilvoorde
+Verviers
+Turnhout
+Tubize
+Tournai
+Torhout
+Tongeren
+Tienen
+Tielt
+Tessenderlo
+Tervuren
+Temse
+Stekene
+Stabroek
+Soumagne
+Soignies
+Sint-Truiden
+Sint-Pieters-Leeuw
+Sint-Niklaas
+Sint-Kruis
+Sint-Katelijne-Waver
+Sint-Gillis-Waas
+Sint-Genesius-Rode
+Seraing
+Schoten
+Schilde
+Saint-Nicolas
+Saint-Ghislain
+Rotselaar
+Ronse
+Roeselare
+Rixensart
+Riemst
+Ranst
+Quaregnon
+Puurs
+Putte
+Poperinge
+Pont-à-Celles
+Péruwelz
+Peer
+Overijse
+Oupeye
+Oudenaarde
+Oostkamp
+Ostend
+Nivelles
+Ninove
+Nijlen
+Neerpelt
+Namur
+Mouscron
+Mortsel
+Morlanwelz-Mariemont
+Mons
+Mol
+Middelkerke
+Merelbeke
+Menen
+Meise
+Mechelen
+Marche-en-Famenne
+Manage
+Maldegem
+Maasmechelen
+Maaseik
+Louvain-la-Neuve
+Londerzeel
+Lommel
+Lokeren
+Lochristi
+Lille
+Lier
+Liège
+Leuven
+Lessines
+Lede
+Lebbeke
+Lanaken
+La Louvière
+Kortrijk
+Kortenberg
+Kontich
+Koksijde
+Knokke-Heist
+Kasterlee
+Kapellen
+Kalmthout
+Izegem
+Ieper
+Huy
+Houthalen
+Hoogstraten
+Hoboken
+Heusden
+Herzele
+Herve
+Herstal
+Herentals
+Herent
+Helchteren
+Heist-op-den-Berg
+Hasselt
+Harelbeke
+Hamme
+Halle
+Haaltert
+Grimbergen
+Geraardsbergen
+Gent
+Genk
+Gembloux
+Geel
+Frameries
+Fleurus
+Fléron
+Flémalle-Haute
+Evergem
+Eupen
+Essen
+Eeklo
+Edegem
+Duffel
+Dour
+Dilbeek
+Diksmuide
+Diest
+Diepenbeek
+Destelbergen
+Dendermonde
+Denderleeuw
+Deinze
+Courcelles
+Colfontaine
+Chaudfontaine
+Châtelet
+Chasse Royale
+Charleroi
+Brussels
+Brugge
+Brecht
+Brasschaat
+Braine-le-Comte
+Braine-l'Alleud
+Boussu
+Bornem
+Boom
+Blankenberge
+Binche
+Bilzen
+Beveren
+Beringen
+Beersel
+Beerse
+Balen
+Ath
+Asse
+Arlon
+Antwerpen
+Ans
+Andenne
+Aarschot
+Aalter
+Aalst
+Zorgo
+Yako
+Tougan
+Titao
+Tenkodogo
+Réo
+Pô
+Ouahigouya
+Ouagadougou
+Nouna
+Manga
+Léo
+Koupéla
+Koudougou
+Kongoussi
+Kombissiri
+Kokologo
+Kaya
+Houndé
+Gourcy
+Garango
+Fada N'gourma
+Dori
+Djibo
+Diapaga
+Dédougou
+Boussé
+Boulsa
+Bobo-Dioulasso
+Banfora
+Gaoua
+Orodara
+Yambol
+Vratsa
+Vidin
+Velingrad
+Veliko Tŭrnovo
+Varna
+Targovishte
+Troyan
+Dobrich
+Svishtov
+Svilengrad
+Stara Zagora
+Dupnitsa
+Sofia
+Smolyan
+Sliven
+Silistra
+Shumen
+Sevlievo
+Sandanski
+Samokov
+Ruse
+Razgrad
+Rakovski
+Popovo
+Plovdiv
+Pleven
+Petrich
+Peshtera
+Pernik
+Pazardzhik
+Panagyurishte
